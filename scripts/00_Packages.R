94117bcb28dcb228de927d71dc1b36b525d5d019
Frankie Gerraty <frankiegerraty@gmail.com>
2023-09-28 11:45:19 -0700

    remove package

--- scripts/00_Packages.R
+++ scripts/00_Packages.R
@@ -8,7 +8,7 @@
 # Part 1: Load Packages --------------------------------------------------
 
 # Load packages
-packages<- c("tidyverse", "readr", "janitor", "raster", "rasterVis", "sf", "mapview", "ggspatial", "mapsf", "car", "ggthemes", "lme4", "cowplot", "lubridate", "vegan", "scatterplot3d", "ggthemes", "ggh4x", "MASS", "AICcmodavg", "mvabund", "gt", "webshot2", "glmmTMB", "DHARMa", "wesanderson", "AICcPermanova", "maps", "emmeans", "waffle", "ggwaffle")
+packages<- c("tidyverse", "readr", "janitor", "raster", "rasterVis", "sf", "mapview", "ggspatial", "mapsf", "car", "ggthemes", "lme4", "cowplot", "lubridate", "vegan", "scatterplot3d", "ggthemes", "ggh4x", "MASS", "AICcmodavg", "mvabund", "gt", "webshot2", "glmmTMB", "DHARMa", "wesanderson", "AICcPermanova", "maps", "emmeans", "waffle")
 
 pacman::p_load(packages, character.only = TRUE)
 
